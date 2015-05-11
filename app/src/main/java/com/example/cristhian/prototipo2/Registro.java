44a9cb9fd7e94677315acf37b142c5bc05b60bac
cristhian <cristhian.leonlizarazo@gmail.com>
2015-05-11 06:52:43 -0500

    elimine el sdk de fb, debido a errore en la desactualizacion

--- app/src/main/java/com/example/cristhian/prototipo2/Registro.java
+++ app/src/main/java/com/example/cristhian/prototipo2/Registro.java
@@ -1,24 +1,10 @@
 package com.example.cristhian.prototipo2;
 
-import android.annotation.TargetApi;
-import android.app.Fragment;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import java.util.Arrays;
 
 /**
  * Created by MAIS on 07/05/2015.
@@ -27,36 +13,11 @@ import java.util.Arrays;
 public class Registro extends ActionBarActivity{
 
 
-    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_registro);
-
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        //authButton.setFragment(this);
-        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
-
-        /*
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-            }
-
-            @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-            }
-        });
-         */
 
     }
 
