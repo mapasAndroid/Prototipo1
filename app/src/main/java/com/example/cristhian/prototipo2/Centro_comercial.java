e0cd658b1bf67e9181dcab3adbce6f887410b15b
mibobrek <mibobrek@gmail.com>
2015-05-17 18:03:16 -0500

    IMAGENES

--- app/src/main/java/com/example/cristhian/prototipo2/Centro_comercial.java
+++ app/src/main/java/com/example/cristhian/prototipo2/Centro_comercial.java
@@ -1,14 +1,19 @@
 package com.example.cristhian.prototipo2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageButton;
 
 /**
  * Created by MAIS on 17/05/2015.
  */
+
+
 public class Centro_comercial extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,5 +21,11 @@ public class Centro_comercial extends Fragment {
 
 View rootView = inflater.inflate(R.layout.lay_centro,container,false);
         return rootView;
+
+
+
+
     }
+
+
 }
