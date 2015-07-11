e9cbd094e3cde91db0727c87dda1100be5108e56
mibobrek <mibobrek@gmail.com>
2015-07-11 16:39:01 -0500

    basesde datos

---
+++ app/src/main/java/com/example/cristhian/prototipo2/BaseDeDatos.java
@@ -0,0 +1,29 @@
+package com.example.cristhian.prototipo2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by MAIS on 11/07/2015.
+ */
+public class BaseDeDatos {
+
+
+    private DBHelper nDBHelper;
+    private Context contexto;
+    SQLiteDatabase nBaseDatos;
+
+    public BaseDeDatos(Context contexto){
+        this.contexto = contexto;
+    }
+
+    public BaseDeDatos abrir(){
+        this.nDBHelper = new DBHelper(this.contexto);
+        this.nBaseDatos = this.nDBHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void cerrar() {
+        this.nDBHelper.close();
+    }
+}
--- app/src/main/java/com/example/cristhian/prototipo2/MapsActivity.java
+++ app/src/main/java/com/example/cristhian/prototipo2/MapsActivity.java
@@ -2,12 +2,17 @@ package com.example.cristhian.prototipo2;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -26,21 +31,7 @@ public class MapsActivity extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -54,27 +45,126 @@ public class MapsActivity extends FragmentActivity {
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
     private void setUpMap() {
         mMap.setMyLocationEnabled(true);
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        //  setMapaMarker();
+        setMapaMarker();
+    }
 
-        Marker Cristhian = mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(7.827343, -72.463329))
-                .title("CRIS"));
-
-        Marker Maiter = mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(7.897025, -72.499592))
-                .title("MAIS"));
+    public HashMap<String, Marker> mapaMarker = new HashMap<String, Marker>();
 
 
+    public void agregueMarker(double lat, double lng, String titulo) {
+        Marker x = this.mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(lat, lng))
+                        .title(titulo)
+                        .visible(false)
+        );
+        this.mapaMarker.put(titulo, x);
+    }
+
+    public void setMapaMarker() {
+
+        //marcadores estudio
+        agregueMarker(7.884461, -72.5004519, "A1");//unisimon calle 13
+        agregueMarker(7.88229, -72.500593, "A2"); //ageso av 3
+        agregueMarker(7.882154, -72.501313, "A3"); //sagrado corazon av 4
+        agregueMarker(7.884857, -72.502388, "A4"); //carmary av 4
+        agregueMarker(7.882402, -72.501917, "A5"); //gran colombiano av 4
+        agregueMarker(7.885591, -72.499565, "A6"); //biblioteca calle 12
+        agregueMarker(7.88577, -72.49855, "A7"); //nacional de comercio calle 12
+        agregueMarker(7.885558, -72.498103, "A8"); //BBVA av 0
+        agregueMarker(7.885217, -72.498055, "A9"); //bancolombia av 0
+        agregueMarker(7.884637, -72.50011, "A10"); //capilla del carmen calle 13
+        agregueMarker(7.884461, -72.5004519, "A1");//unisimon calle 13
+
+        //centros comerciales, bancos y cajeros
+        agregueMarker(7.88849,-72.505266, "B1");//alejandria av 6
+        agregueMarker(7.887318,-72.505035, "B2"); //plaza de los andes av 6
+        agregueMarker(7.886921,-72.50496, "B3"); //BBVA cajero av 6
+        agregueMarker(7.886921,-72.50496, "B4"); //davivienda banco av 6
+        agregueMarker(7.8859536,-72.5049926, "B5"); //santander banco av 6
+        agregueMarker(7.88514,-72.50342, "B6"); //cajero ancolombia av 5
+        agregueMarker(7.88509,-72.503855, "B7"); //exito av 5
+        agregueMarker(7.8852947,-72.5038695, "B8"); //banco popular av 5
+        agregueMarker(7.8859988,-72.50353625, "B9"); //av villas calle 11 banco
+        agregueMarker(7.886122,-72.5029179, "B10"); //cajero ath calle 11
+        agregueMarker(7.8855549,-72.503075, "B11"); //lecs centro comercial av 4
+        agregueMarker(7.8879759,-72.5039625, "B12"); //rayotex calle 9
+        agregueMarker(7.8879246,-72.5037372, "B13"); //river plaza centro comercial calle 9
+        agregueMarker(7.8880609,-72.5033699, "B14"); //bbva calle 9 cajero
+        agregueMarker(7.88849,-72.505266, "B1");//alejandria av 6
+
+        //parques
+        agregueMarker(7.885067,-72.500351, "C1");//parque colon av 2
+        agregueMarker(7.887344,-72.501511, "C2"); //hotel acora calle 10
+        agregueMarker(7.887659,-72.501875, "C3"); //hotel efrau av 3
+        agregueMarker(7.8876415,-72.5020174, "C4"); //hotel la bastilla av 3
+        agregueMarker(7.8887201,-72.50241, "C5"); //adpostal calle 8
+        agregueMarker(7.888245,-72.503285, "C6"); //bbva calle 9
+        agregueMarker(7.8879762,-72.5039627, "C7"); //rayotex calle 9
+        agregueMarker(7.88514,-72.50342, "C8"); //bancolombia cajero av 5
+        agregueMarker(7.8863475,-72.504373, "C9"); //parque santander calle 10
+        agregueMarker(7.88743,-72.50526, "C10"); //plaza de los andes av 6
+        agregueMarker(7.88797,-72.50548, "C11"); //alejandria av 6
+        agregueMarker(7.8882618,-72.5069983, "C12"); //atlantis plaza calle 8
+        agregueMarker(7.8879904,-72.5072965, "C13"); //parque mercedes calle 8
+        agregueMarker(7.891818,-72.505613, "C14"); //parque lineal calle 5
+        agregueMarker(7.8919608,-72.5043739, "C15"); //representaciones orozco calle 5
+        agregueMarker(7.8934246,-72.5032076, "C16"); //estacion de servicio los amigos calle 4
+        agregueMarker(7.8934246,-72.5032076, "C17"); //u pamplona diagonal
+        agregueMarker(7.8923437,-72.5017718, "C18"); //efecty av 2
+        agregueMarker(7.885067,-72.500351, "C1");//parque colon av 2
+
+
+        //hoteles y restaurantes
+        agregueMarker(7.8841587,-72.4986827, "D1");//pachoricuras av 1
+        agregueMarker(7.8849064,-72.499263, "D2"); //biblioteca av 1
+        agregueMarker(7.886646,-72.5009548, "D3"); //hotel acora calle 10
+        agregueMarker(7.8875916,-72.501719, "D3"); //hotel efrau av 3
+        agregueMarker(7.8867775,-72.5019828, "D4"); //la bastilla av 3
+        agregueMarker(7.8876952,-72.5036797, "D5"); //la mazorca av 4
+        agregueMarker(7.8873688,-72.503484, "D6"); //sonricenter av 4
+        agregueMarker(7.8881263,-72.5023979, "D7"); //pasteleria la mejor calle 9
+        agregueMarker(7.8878918,-72.5044093, "D8"); //amaruc calle 10
+        agregueMarker(7.8870899,-72.5035129, "D9"); //bancolombia av 5
+        agregueMarker(7.8878918,-72.5044093, "D10"); //pasteleria araña de oro calle 9
+        agregueMarker(7.8873253,-72.5047462, "D11"); //plaza de los andes av 6
+        agregueMarker(7.8861056,-72.5045606, "D12"); //banco santander av 6
+        agregueMarker(7.8833239,-72.50037893, "D14"); //copy copy calle 14
+        agregueMarker(7.8835552,-72.4993124, "D15"); //serventel baloto calle 14
+        agregueMarker(7.8841587,-72.4986827, "D1");//pachoricuras av 1
 
 
     }
+
+    public void muestre(String tipo) {
+        Iterator x = this.mapaMarker.entrySet().iterator();
+        while (x.hasNext()) {
+            Map.Entry temporal = (Map.Entry) x.next();
+
+            Marker marcador = (Marker) temporal.getValue();
+            if (marcador.getTitle().startsWith(tipo)) {
+                marcador.setVisible(true);
+            } else {
+                marcador.setVisible(false);
+            }
+        }
+    }
+
+    public void getA(View view) {muestre("A");}
+
+    public void getB(View view) {
+        muestre("B");
+    }
+
+    public void getC(View view) {
+        muestre("C");
+    }
+
+    public void getD(View view) {
+        muestre("D");
+    }
+
+
 }
